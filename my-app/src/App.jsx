5b1ac52ad4bfdeb75334092b6f40f089138f6084
Ami Fukuyama <amifuku80@gmail.com>
2025-11-11 16:25:43 +0800

    Setup vite and css

---
+++ my-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Home.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import HeroImage from "./images/cafe-hero.jpg";
+
+export default function Home() {
+  return (
+    <div className="relative w-full h-screen object-cover">
+      <div className="absolute w-full h-screen bg-linear-to-b from-black via-black/40 to-transparent z-10"></div>
+      <img
+        src={HeroImage}
+        alt="Cafe Hero"
+        className="absolute inset-0 w-full h-full object-cover z-0"
+      ></img>
+      <div className="absolute inset-0 flex flex-col justify-center items-center text-center z-20">
+        <h1 className="text-white font-aboreto text-5xl mb-2">Stone Street</h1>
+        <h2 className="text-white font-sacramento text-5xl">cafe</h2>
+      </div>
+    </div>
+  );
+}
